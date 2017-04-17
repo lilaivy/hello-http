406a8e5f8b10629d0f9291c5878e258e7db0e37d
Ivy Patton <lilaivy@gmail.com>
2017-04-17 14:25:18 -0700

    Set up environment and basic server

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./lib/app');
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port, () => {
+    console.log('server running on port', server.address().port);
+})
