7ffcb43502256bd1eb37483593067f5b4009b8e8
Ivy Patton <lilaivy@gmail.com>
2017-04-17 17:27:05 -0700

    passing tests for hello function

---
+++ test/app.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+const app = require('../lib/app');
+
+chai.use(chaiHttp);
+
+describe('app', () => {
+
+    const request = chai.request(app);
+
+    it('says hello ivy when name passed as query param', done => {
+        request.get('/greeting/Ivy')
+            .end((err, res) => {
+                assert.equal(res.text, '<h1>Hello, Ivy!</h1>');
+                done();
+            });
+    });
+
+    it('says hello stranger when no name is passed in', done => {
+        request.get('/greeting')
+            .end((err, res) => {
+                assert.equal(res.text, '<h1>Hello, Stranger!</h1>');
+                done();
+            });
+    });
+});
